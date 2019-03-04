dd422fe302331857ac235fa18f6da12e70a9a129
Ling Zhang <ling.zhang@nytimes.com>
2019-03-04 12:30:28 -0500

    try print out the key

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cleanup-script
 
 ⚠️ This repo is public
+ 
+run `pip install pygithub` before you compile the file
--- users.py
+++ users.py
@@ -5,11 +5,18 @@ from github import Github
 from urllib.parse import urlsplit
 
 # Read token from env var
-if 'GITHUB_TOKEN' not in os.environ:
-    print('Environment variable "GITHUB_TOKEN" required.')
+if 'GITHUB_ORG_READ' not in os.environ:
+    print('Environment variable "GITHUB_ORG_READ" required.')
     sys.exit(1)
 
-g = Github(os.environ['GITHUB_TOKEN'])
+g = Github(os.environ['GITHUB_ORG_READ'])
 
 # Get org
 nyt = g.get_organization('nytimes')
+
+# Get teams list
+nyt_repos = nyt.get_repos(type="all")
+for repo in nyt_repos:
+    deploy_keys = repo.get_keys()
+    for key in deploy_keys:
+        print (repo.name, ',key_id: ', key.id, ', title: ', key.title, ', created_at: ', key.created_at )
